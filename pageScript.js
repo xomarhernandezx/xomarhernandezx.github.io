b7ebe9d117507a1fd8e5c180cabbfb49739ca812
Omar <bhoa1990@gmail.com>
2021-07-30 23:47:52 -0500

    Se elimina función felcha, compatibilidad IOS

--- pageScript.js
+++ pageScript.js
@@ -1,22 +1,20 @@
 const pos_nav = document.documentElement;
 const pos_mov = document.documentElement;
 
-pos_nav.addEventListener("mousemove", (e) => {
+pos_nav.addEventListener("mousemove", function(e){
   pos_nav.style.setProperty("--x", e.clientX + "px");
   pos_nav.style.setProperty("--y", e.clientY + "px");
   /*Variables incrustadas en el documento*/
 });
 
-pos_mov.addEventListener("touchmove", (e)=>{
+pos_mov.addEventListener("touchmove", function(e){
   pos_mov.style.setProperty("--x", e.touches[0].pageX + "px");
   pos_mov.style.setProperty("--y", e.touches[0].pageY + "px");
-  console.log(e.touches[0].pageX);
-  console.log(e.touches[0].pageY);
   /*Variables incrustadas en el documento*/
 });
 
 $(document).ready(function () {
-  $(document).mousemove(function (e) { 
+  $(document).mousemove(function () { 
     $("#back").css("clipPath", "circle(150px at var(--x) var(--y))");
     $("#back").css("background", "var(--obscuro)");
     $("#focus").css("boxShadow", "0 0 3px var(--claro), 0 0 6px var(--claro), 0 0 9px var(--claro)");
