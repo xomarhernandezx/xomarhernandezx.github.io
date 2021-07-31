8d89596452f6ba05b5bc0b3f37d62e54628f0721
Omar <bhoa1990@gmail.com>
2021-07-30 23:33:24 -0500

    subida de archivos

---
+++ pageScript.js
@@ -0,0 +1,49 @@
+const pos_nav = document.documentElement;
+const pos_mov = document.documentElement;
+
+pos_nav.addEventListener("mousemove", (e) => {
+  pos_nav.style.setProperty("--x", e.clientX + "px");
+  pos_nav.style.setProperty("--y", e.clientY + "px");
+  /*Variables incrustadas en el documento*/
+});
+
+pos_mov.addEventListener("touchmove", (e)=>{
+  pos_mov.style.setProperty("--x", e.touches[0].pageX + "px");
+  pos_mov.style.setProperty("--y", e.touches[0].pageY + "px");
+  console.log(e.touches[0].pageX);
+  console.log(e.touches[0].pageY);
+  /*Variables incrustadas en el documento*/
+});
+
+$(document).ready(function () {
+  $(document).mousemove(function (e) { 
+    $("#back").css("clipPath", "circle(150px at var(--x) var(--y))");
+    $("#back").css("background", "var(--obscuro)");
+    $("#focus").css("boxShadow", "0 0 3px var(--claro), 0 0 6px var(--claro), 0 0 9px var(--claro)");
+    /* Se agregan estilos CSS al ingresar al documento */
+  });
+});
+$(document).ready(function () {
+  $(document).on("touchstart", function () {
+    $("#back").css("clipPath", "circle(150px at var(--x) var(--y))");
+    $("#back").css("background", "var(--obscuro)");
+    $("#focus").css("boxShadow", "0 0 3px var(--claro), 0 0 6px var(--claro), 0 0 9px var(--claro)");
+  });
+});
+
+$(document).ready(function () {
+  $(document).mouseleave(function () { 
+    $("#back").css("clipPath", "");
+    $("#back").css("background", "");
+    $("#focus").css("boxShadow", "");
+    /* Se cambian estilos CSS al salir del documento */
+  });
+});
+
+$(document).ready(function () {
+  $(document).on("touchend", function () {
+    $("#back").css("clipPath", "");
+    $("#back").css("background", "");
+    $("#focus").css("boxShadow", "");
+  });
+});
