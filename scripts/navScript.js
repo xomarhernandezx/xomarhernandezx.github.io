d524571554608591c0998baccf879c9436cabf11
Omar <bhoa1990@gmail.com>
2021-08-18 00:02:52 -0500

    Se agregan script para direccionar a pagina de proyectos , contacto y perfiil, se agregan estilos para navegador movil y navegador de escritorio

--- scripts/navScript.js
+++ scripts/navScript.js
@@ -2,28 +2,59 @@ $(document).ready(function () {
   $("#logoButton").click(function (e) { 
     $("#logoButton").toggleClass("rotate");
     $("#navMov").toggleClass("translate");
+    $("#navMov").toggleClass("navMovBack");
+    $("#navDesk").toggleClass("navDeskBack");
     $("#aboutDesk").toggleClass("translateA");
     $("#contactDesk").toggleClass("translateC");
     $("#projectsDesk").toggleClass("translateP");
     $("#tipsDesk").toggleClass("translateT");
     $("#footerDesk").toggleClass("translateF");
+
   });
 });
+/* Funciones */
+function dirA() {
+  $(location).attr("href", "about.html");
+}
+function dirP() {
+  $(location).attr("href", "project.html");
+}
+function dirC() {
+  $(location).attr("href", "contact.html");
+}
+
+/* Escritorio */
+
 /* Se redirecciona a ruta about.html */
 $("#aboutDesk").click(function (e) {
   $("#loader").css("display", "grid");
-  setTimeout(dir,3000);
+  setTimeout(dirA,3000);
 });
-
-function dir() {
-  $(location).attr("href", "about.html");
-}
 /* Se redirecciona a ruta project.html */
-$("#aboutDesk").click(function (e) {
+$("#projectsDesk").click(function (e) {
   $("#loader").css("display", "grid");
-  setTimeout(dir,3000);
+  setTimeout(dirP,3000);
+});
+/* Se redirecciona a ruta contact.html */
+$("#contactDesk").click(function (e) {
+  $("#loader").css("display", "grid");
+  setTimeout(dirC,3000);
 });
 
-function dir() {
-  $(location).attr("href", "about.html");
-}
+/* Movil */
+
+/* Se redirecciona a ruta about.html */
+$("#aboutMov").click(function (e) {
+  $("#loader").css("display", "grid");
+  setTimeout(dirA,3000);
+});
+/* Se redirecciona a ruta project.html */
+$("#projectsMov").click(function (e) {
+  $("#loader").css("display", "grid");
+  setTimeout(dirP,3000);
+});
+/* Se redirecciona a ruta contact.html */
+$("#contactMov").click(function (e) {
+  $("#loader").css("display", "grid");
+  setTimeout(dirC,3000);
+});
