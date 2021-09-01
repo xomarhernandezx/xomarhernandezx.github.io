27211bf6f2b712b246de06c658e064d21e0187e8
Omar <bhoa1990@gmail.com>
2021-09-01 01:43:54 -0500

    Se corrigen estilos y script del navegador

--- scripts/navScript.js
+++ scripts/navScript.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
   $("#logoButton").click(function (e) { 
     $("#logoButton").toggleClass("rotate");
+    $("#navMov").toggleClass("zindex", "true");
     $("#aboutMov").toggleClass("translateTB");
     $("#contactMov").toggleClass("translateRL");
     $("#projectsMov").toggleClass("translateLR");
     $("#footerMov").toggleClass("translateBT");
     $("#navMov").toggleClass("navMovBack");
+    $("#navDesk").toggleClass("zindex", "true");
     $("#navDesk").toggleClass("navDeskBack");
     $("#aboutDesk").toggleClass("translateLR");
     $("#contactDesk").toggleClass("translateBT");
@@ -61,3 +63,4 @@ $("#contactMov").click(function (e) {
   $("#preloader").css("display", "grid");
   setTimeout(dirC,1000);
 });
+
