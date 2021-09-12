8b66f6b341123057c5d637ecfeacad210d162753
Omar <bhoa1990@gmail.com>
2021-09-12 00:34:37 -0500

    Se optimiza código en archivo nav, se agrega fondo

--- scripts/navScript.js
+++ scripts/navScript.js
@@ -1,18 +1,29 @@
 $(document).ready(function () {
-  $("#logoButton").click(function (e) { 
+  $("#logoButton").click(function (e) {
+    let navDesk = document.getElementById("navDesk");
+    let navMov = document.getElementById("navMov");
+
+    $("section").toggleClass("filterBlur");
+    $("footer").toggleClass("filterBlur");
     $("#logoButton").toggleClass("rotate");
-    $("#aboutMov").toggleClass("translateTB");
-    $("#contactMov").toggleClass("translateRL");
-    $("#projectsMov").toggleClass("translateLR");
-    $("#footerMov").toggleClass("translateBT");
-    $("#navMov").toggleClass("navMovBack");
+
+    navDesk.style.display = (navDesk.dataset.toggled ^= 1) ? "grid" : "none";
+
     $("#navDesk").toggleClass("navDeskBack");
     $("#aboutDesk").toggleClass("translateLR");
     $("#contactDesk").toggleClass("translateBT");
     $("#projectsDesk").toggleClass("translateTB");
     $("#footerDesk").toggleClass("translateRL");
-    $("section").toggleClass("filterBlur");
-    $("footer").toggleClass("filterBlur");
+    // Sección de escritorio
+
+    navMov.style.display = (navMov.dataset.toggled ^= 1) ? "grid" : "none";
+
+    $("#navMov").toggleClass("navMovBack");
+    $("#aboutMov").toggleClass("translateTB");
+    $("#contactMov").toggleClass("translateRL");
+    $("#projectsMov").toggleClass("translateLR");
+    $("#footerMov").toggleClass("translateBT");
+    // Sección de movil
   });
 });
 /* Funciones */
@@ -31,17 +42,17 @@ function dirC() {
 /* Se redirecciona a ruta about.html */
 $("#aboutDesk").click(function (e) {
   $("#preloader").css("display", "grid");
-  setTimeout(dirA,1000);
+  setTimeout(dirA, 1000);
 });
 /* Se redirecciona a ruta project.html */
 $("#projectsDesk").click(function (e) {
   $("#preloader").css("display", "grid");
-  setTimeout(dirP,1000);
+  setTimeout(dirP, 1000);
 });
 /* Se redirecciona a ruta contact.html */
 $("#contactDesk").click(function (e) {
   $("#preloader").css("display", "grid");
-  setTimeout(dirC,1000);
+  setTimeout(dirC, 1000);
 });
 
 /* Movil */
@@ -49,16 +60,15 @@ $("#contactDesk").click(function (e) {
 /* Se redirecciona a ruta about.html */
 $("#aboutMov").click(function (e) {
   $("#preloader").css("display", "grid");
-  setTimeout(dirA,1000);
+  setTimeout(dirA, 1000);
 });
 /* Se redirecciona a ruta project.html */
 $("#projectsMov").click(function (e) {
   $("#preloader").css("display", "grid");
-  setTimeout(dirP,1000);
+  setTimeout(dirP, 1000);
 });
 /* Se redirecciona a ruta contact.html */
 $("#contactMov").click(function (e) {
   $("#preloader").css("display", "grid");
-  setTimeout(dirC,1000);
+  setTimeout(dirC, 1000);
 });
-
